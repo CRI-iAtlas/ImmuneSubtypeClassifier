f5faaa4e7e1cf9f6224e89df879cb50f007917a8
David L Gibbs <gibbsdavidl@gmail.com>
2019-06-07 09:21:47 -0700

    testing gdc data

--- test/test_gdc.R
+++ test/test_gdc.R
@@ -1,3 +1,9 @@
 
 # Test on GDC data #
 
+
+library(devtools)
+library(readr)
+
+devtools::install_github("Gibbsdavidl/ImmuneSubtypeClassifier", force = T)
+reload(pkgload::inst('ImmuneSubtypeClassifier'))
